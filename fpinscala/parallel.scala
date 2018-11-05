e79a01d2efdc8edf8697019e704c52bb93ffb7b2
Yiqiao <yiqiao.wang96@gmail.com>
2018-11-05 07:07:49 -0500

    Exercise 7.2

--- fpinscala/parallel.scala
+++ fpinscala/parallel.scala
@@ -1,40 +1,44 @@
+// sealed trait Par[+A] {
+//   def unit[A](a: => A): Par[A]
+//   def get[A](a: => Par[A]): A
+//   def run[A](a: => Par[A]): A
+//   def fork[A](a: => Par[A]): Par[A]
+//   def lazyUnit[A](a: => A): Par[A] = fort(unit(a))
+//   def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C]
+// }
+
 // object Parallel {
 //   def sum(ints: Seq[Int]): Int =
-//     ints.foldLeft(0)((a, b) => a + b)
+//     ints.foldLeft(0)(_ + _)
 
-//   def sum_dc(ints: IndexedSeq[Int]): Int = {
+//   def sum_old(ints: IndexedSeq[Int]): Int =
 //     if (ints.size <= 1)
 //       ints.headOption getOrElse 0
 //     else {
-//       val (l, r) = ints.splitAt(ints.length / 2) 
-//       sum_dc(l) + sum_dc(r)
+//       val (l, r) = ints.splitAt(ints.length/2)
+//       sum_old(l) + sum_old(r)
 //     }
-//   }
 
-
-//   def _sum_par(ints: IndexedSeq[Int]): Int = {
+//   def sum(ints: IndexedSeq[Int]): Int =
 //     if (ints.size <= 1)
-//       ints headOption getOrElse 0
+//       ints.headOption getOrElse 0
 //     else {
-//       val (l, r) = ints.splitAt(ints.length / 2)
+//       val (l, r) = ints.splitAt(ints.length/2)
 //       val sumL: Par[Int] = Par.unit(sum(l))
 //       val sumR: Par[Int] = Par.unit(sum(r))
 //       Par.get(sumL) + Par.get(sumR)
 //     }
-//   }
-
-//   def sum_par(ints: IndexedSeq[Int]): Par[Int] = {
-//     if (ints.size <= 1)
-//       Par.unit(ints.headOption getOrElse 0)
-//     else {
-//       val (l, r) = ints.splitAt(ints.length / 2)
-//       Par.map2(sum(1), sum(2))(_ + _)
-//     }
-//   }
-
-//   // Type signature for Par.map2 is something similar to  ->
-//   // def map2[A, B, C](a: Par[A], b: Par[B], f:(A, B) => C): C
-
-//   // unit creates the parallel computation
-//   // get executes the parallel computation
 // }
+
+
+/**
+ *
+ * Signature for Par.map2
+ * def Par.map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C]
+ *
+ * */
+
+
+/*              Exercise 7.2               */
+
+
